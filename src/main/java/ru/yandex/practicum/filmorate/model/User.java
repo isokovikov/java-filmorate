298a7078c14abb8d917962360f24a1c437acef25
isokovikov <i.sokovikov@gs1ru.org>
2024-02-01 10:43:20 +0300

    fix: User.java:9:1: 'CLASS_DEF' should be separated from previous line.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
+
 @Data
 public class User {
     private Integer id;
