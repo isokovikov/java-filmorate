6f21fbdf1a0e6d6211346f6b39e3283e2a5b8f4b
isokovikov <i.sokovikov@gs1ru.org>
2024-02-01 10:40:27 +0300

    fix: Film.java:5:8: Unused import - jakarta.validation.constraints.NotNull

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
