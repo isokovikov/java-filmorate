58f6d3241507b70e21815346ba814046533e9bd9
isokovikov <i.sokovikov@gs1ru.org>
2024-02-03 14:31:15 +0300

    fix: ТЗ-10 UnusedImports

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
     User addNew(User user);
